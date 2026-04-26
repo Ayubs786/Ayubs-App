af310d85e02718c94f83dfcb465c7aafc16889d7
Ayubs786 <ayub@holistica.co.uk>
2026-04-26 10:22:12 +0100

    Update page.js

--- daily-companion/app/page.js
+++ daily-companion/app/page.js
@@ -1,11 +1,5 @@
-import { isAuthenticated } from '@/lib/auth';
-import LoginScreen from './components/LoginScreen';
 import DailyCompanion from './components/DailyCompanion';
 
 export default function Page() {
-  const authed = isAuthenticated();
-  if (!authed) {
-    return <LoginScreen />;
-  }
   return <DailyCompanion />;
 }
